d3f7e2bb582feca59da32d7b795ffd0d83463a98
Léa Mhoumadi <95865130+Leambr@users.noreply.github.com>
2024-02-15 16:03:30 +0100

    Drawer and User Menu (#7)
    * feat: add drawer menu
    
    * feat: add new primary color and update drawer menu
    
    * refactor: fix navbar error on reload
    
    * feat: display user menu
    
    * refactor: fix navbar display on sign in

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,35 +1,46 @@
-import React from 'react';
-import { ViewHeadline } from '@mui/icons-material';
+import React, { useState } from 'react';
+import { ArrowDropDown, ArrowDropUp, ViewHeadline } from '@mui/icons-material';
 import { AppBar, Avatar, Box, Toolbar, Typography } from '@mui/material';
 import s from './Navbar.module.css';
+import { DashboardMenu } from './DashboardMenu/DashboardMenu';
+import { UserMenu } from './UserMenu/UserMenu';
 
 export default function Navbar() {
-    // const [displayLeftMenu, setDisplayLeftMenu] = useState(false);
+    const [displayLeftMenu, setDisplayLeftMenu] = useState(false);
+    const [displayUserMenu, setDisplayUserMenu] = useState(false);
+    const [userMenuAnchor, setUserMenuAnchor] = useState<null | HTMLElement>(null);
+
+    const handleUserMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+        setUserMenuAnchor(event.currentTarget);
+        setDisplayUserMenu(true);
+    };
 
     return (
         <Box>
             <AppBar position="static">
-                <Toolbar variant="dense" className={s.navbar}>
-                    <div className={s.burgerMenu}>
+                <Toolbar variant="dense">
+                    <div className={s.burgerMenu} onClick={() => setDisplayLeftMenu(true)}>
                         <ViewHeadline />
                     </div>
+                    <DashboardMenu
+                        isOpen={displayLeftMenu}
+                        onClose={() => setDisplayLeftMenu(false)}
+                    />
                     <Typography variant="titleS">E-shop</Typography>
                     <div className={s.rightContainer}>
-                        <div className={s.userMenu}>
-                            <Avatar alt="Billy Boy" />
-                            {/* {displayUserMenu ? (
+                        <div className={s.userMenu} onClick={handleUserMenuClick}>
+                            <Avatar />
+                            {displayUserMenu ? (
                                 <ArrowDropUp sx={{ margin: 1 }}> </ArrowDropUp>
                             ) : (
                                 <ArrowDropDown sx={{ margin: 1 }}></ArrowDropDown>
-                            )} */}
+                            )}
                         </div>
-                        {/* <UserMenu
+                        <UserMenu
                             isOpen={displayUserMenu}
                             anchorElement={userMenuAnchor}
-                            fullName={userFullName}
-                            avatar={userAvatar}
                             onClose={() => setDisplayUserMenu(false)}
-                        /> */}
+                        />
                     </div>
                 </Toolbar>
             </AppBar>
