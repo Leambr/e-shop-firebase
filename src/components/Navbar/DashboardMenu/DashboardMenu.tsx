f91ba814f016d181310a3cd7c483e4def14440ba
Léa Mhoumadi <mhoumadi.lea@gmail.com>
2024-02-15 15:06:37 +0100

    feat: add drawer menu

---
+++ src/components/Navbar/DashboardMenu/DashboardMenu.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Close } from '@mui/icons-material';
+import {
+    Box,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    Paper,
+    Typography,
+    useMediaQuery,
+} from '@mui/material';
+import s from './DashboardMenu.module.css';
+
+interface DashboardMenuProps {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export const DashboardMenu = ({ isOpen, onClose }: DashboardMenuProps) => {
+    const isMobile = useMediaQuery('(max-width: 720px)');
+    return (
+        <Drawer
+            anchor="left"
+            open={isOpen}
+            onClose={onClose}
+            PaperProps={{
+                style: { width: isMobile ? '100%' : 300 },
+            }}
+        >
+            <Paper elevation={0}>
+                <Box className={s.menuHeader}>
+                    <div className={s.headerContent}>
+                        <Typography variant="titleM">E-shop Menu</Typography>
+                    </div>
+                    <IconButton
+                        onClick={onClose}
+                        sx={{
+                            color: 'var(--color-neutral-900)',
+                            alignItems: 'flex-start',
+                            paddingTop: '0px',
+                            display: isMobile ? 'flex' : 'none',
+                        }}
+                    >
+                        <Close />
+                    </IconButton>
+                </Box>
+                <div className={s.listContainer}>
+                    <List
+                        sx={{
+                            marginBottom: '32px',
+                            padding: 0,
+                        }}
+                    >
+                        <ListItem
+                            sx={{
+                                padding: '0 0 0 24px',
+                                marginBottom: '4px',
+                            }}
+                        >
+                            <Typography variant="badge" color={'grey'}>
+                                Product
+                            </Typography>
+                        </ListItem>
+                        <ListItemButton
+                            onClick={onClose}
+                            //mettre la bonne route pour rediriger sur la bonne page
+                            href=""
+                            sx={{
+                                padding: '12px 32px',
+                            }}
+                        >
+                            Products
+                        </ListItemButton>
+                        <ListItemButton
+                            onClick={onClose}
+                            href=""
+                            sx={{
+                                padding: '12px 32px',
+                            }}
+                        >
+                            Basket
+                        </ListItemButton>
+                    </List>
+                </div>
+            </Paper>
+        </Drawer>
+    );
+};
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ViewHeadline } from '@mui/icons-material';
 import { AppBar, Avatar, Box, Toolbar, Typography } from '@mui/material';
 import s from './Navbar.module.css';
+import { DashboardMenu } from './DashboardMenu/DashboardMenu';
 
 export default function Navbar() {
-    // const [displayLeftMenu, setDisplayLeftMenu] = useState(false);
+    const [displayLeftMenu, setDisplayLeftMenu] = useState(false);
 
     return (
         <Box>
             <AppBar position="static">
-                <Toolbar variant="dense" className={s.navbar}>
-                    <div className={s.burgerMenu}>
+                <Toolbar variant="dense">
+                    <div className={s.burgerMenu} onClick={() => setDisplayLeftMenu(true)}>
                         <ViewHeadline />
                     </div>
+                    <DashboardMenu
+                        isOpen={displayLeftMenu}
+                        onClose={() => setDisplayLeftMenu(false)}
+                    />
                     <Typography variant="titleS">E-shop</Typography>
                     <div className={s.rightContainer}>
                         <div className={s.userMenu}>
